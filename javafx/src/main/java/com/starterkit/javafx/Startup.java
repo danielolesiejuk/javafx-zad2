cea6863e815f6159038faf312952326b8c5be457
danielolesiejuk <daniel.olesiejuk@gmail.com>
2016-09-23 12:25:38 +0200

    zadanie 2 - starterkit javafx

---
+++ javafx/src/main/java/com/starterkit/javafx/Startup.java
@@ -0,0 +1,44 @@
+package com.starterkit.javafx;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Startup extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	 Stage stage;
+	
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+		
+		String langCode = getParameters().getNamed().get("lang");
+		if (langCode != null && !langCode.isEmpty()) {
+			Locale.setDefault(Locale.forLanguageTag(langCode));
+		}
+		
+        primaryStage.setTitle("StarterKit-JavaFX");
+        
+        Parent root = FXMLLoader.load(getClass().getResource("/com/starterkit/javafx/view/imageBrowser.fxml"), //
+				ResourceBundle.getBundle("com/starterkit/javafx/bundle/base"));
+
+        Scene scene = new Scene(root);
+        
+        scene.getStylesheets().add(getClass().getResource("/com/starterkit/javafx/css/standard.css").toExternalForm());
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    
+}
